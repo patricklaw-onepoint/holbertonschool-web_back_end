621a753dd764f96d17265aad1fb35b215c23f912
Patrick Law <9209@holbertonstudents.com>
2024-12-05 23:39:28 +1100

    NoSQL

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -17,8 +17,25 @@ def log():
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status} status check")
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f"{status_check} status check")
+
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {"_id": 0, "ip": "$_id", "count": 1}},
+    ]
+    commonIps = collection.aggregate(pipeline)
+
+    print("IPs:")
+    for item in commonIps:
+        ip = item.get("ip")
+        count = item.get("count")
+        print(f"\t{ip}: {count}")
 
 
 if __name__ == "__main__":
